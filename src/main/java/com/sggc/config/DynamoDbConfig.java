1c5fb8bc7aad3aded5ba878c42ec8aef0d4f956b
Toby <tobypl407@googlemail.com>
2022-03-26 12:24:32 +0000

    Use default credentials provider as its much more flexible

--- src/main/java/com/sggc/config/DynamoDbConfig.java
+++ src/main/java/com/sggc/config/DynamoDbConfig.java
@@ -1,9 +1,6 @@
 package com.sggc.config;
 
-import com.amazonaws.auth.AWSCredentials;
-import com.amazonaws.auth.AWSCredentialsProvider;
-import com.amazonaws.auth.AWSStaticCredentialsProvider;
-import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.auth.*;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
@@ -19,19 +16,8 @@ import org.springframework.context.annotation.Configuration;
 @EnableDynamoDBRepositories(basePackages = "com.sggc.repositories")
 public class DynamoDbConfig {
 
-    @Value("${amazon.aws.accesskey}")
-    private String amazonAWSAccessKey;
-
-    @Value("${amazon.aws.secretkey}")
-    private String amazonAWSSecretKey;
-
     public AWSCredentialsProvider amazonAWSCredentialsProvider() {
-        return new AWSStaticCredentialsProvider(amazonAWSCredentials());
-    }
-
-    @Bean
-    public AWSCredentials amazonAWSCredentials() {
-        return new BasicAWSCredentials(amazonAWSAccessKey, amazonAWSSecretKey);
+        return new DefaultAWSCredentialsProviderChain();
     }
 
     @Bean
