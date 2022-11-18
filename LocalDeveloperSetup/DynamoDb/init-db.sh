3c28e5bd1864b7212f6a40b761a02c1fb2f1f5f3
Toby <tobypl407@googlemail.com>
2022-11-18 11:50:12 +0000

    Improve the visibility of init-db script log messages

--- LocalDeveloperSetup/DynamoDb/init-db.sh
+++ LocalDeveloperSetup/DynamoDb/init-db.sh
@@ -16,20 +16,20 @@ set aws_access_key_id "DUMMY_ACCESS_KEY" --profile localuser \
 # Set the default profile to the one just created to prevent having to provide it for subsequent AWS commands
 export AWS_PROFILE=localuser
 
-echo "Starting dynamodb local..."
+echo "########## Starting dynamodb local... ##########"
 java -Djava.library.path=./DynamoDBLocal_lib -jar ./DynamoDBLocal.jar -sharedDb &
 DYNAMO_PID=$!
 
-echo "Waiting for dynamodb local..."
+echo "########## Waiting for dynamodb local... ##########"
 while ! timeout 1 bash -c "echo > /dev/tcp/localhost/$DYNAMO_PORT" 2> /dev/null; do
   sleep 1
 done
 
 
-echo "Initializing tables"
+echo "########## Initializing tables ##########"
 for f in sggc-setup/tables/*.json; do
     echo "$0: Creating table from $f"; aws dynamodb create-table --cli-input-json file://$f --endpoint-url $DYNAMO_ENDPOINT_URL
 done
-echo "Tables initialized"
-echo "DB boostrap completed!"
+echo "########## Tables initialized ##########"
+echo "########## DB boostrap completed! ##########"
 wait $DYNAMO_PID
