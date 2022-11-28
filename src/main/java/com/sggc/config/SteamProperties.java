b40550fbeff1eaa30c01950c7596cd8fbc18956d
Toby <tobypl407@googlemail.com>
2022-11-28 14:21:29 +0000

    Add environment variables for Steam API settings

---
+++ src/main/java/com/sggc/config/SteamProperties.java
@@ -0,0 +1,16 @@
+package com.sggc.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+/**
+ * Represents the configuration of the bean used to configure the AWS Secret Manager client for the application
+ */
+@Component
+@ConfigurationProperties("steam")
+@Data
+public class SteamProperties {
+    private String apiAddress;
+    private String storeAddress;
+}
