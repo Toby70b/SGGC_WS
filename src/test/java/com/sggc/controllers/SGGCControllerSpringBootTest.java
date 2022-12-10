3ea749e3cc6aef9e640741e1c8fcd9410bd10890
Toby <tobypl407@googlemail.com>
2022-12-10 14:09:23 +0000

    Fix input validation integration test

--- src/test/java/com/sggc/controllers/SGGCControllerSpringBootTest.java
+++ src/test/java/com/sggc/controllers/SGGCControllerSpringBootTest.java
@@ -4,29 +4,34 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sggc.errors.ApiError;
 import com.sggc.models.sggc.SGGCResponse;
 import com.sggc.models.steam.request.GetCommonGamesRequest;
+import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.*;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import java.util.Collections;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-/*
-TODO: This tests needs to be fixed in the next integration test work
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(controllers = SGGCController.class)
 public class SGGCControllerSpringBootTest {
 
-    private static final ObjectMapper om = new ObjectMapper();
-
-    @LocalServerPort
-    private int port;
+    public static final String SGGC_CONTROLLER_URI = "/api/sggc/";
 
     @Autowired
-    private TestRestTemplate restTemplate;
+    private MockMvc mockMvc;
+    @Autowired
+    private ObjectMapper mapper;
+    @MockBean
+    private SGGCController controller;
 
     @Test
     @DisplayName("Given a request with less than two steam id's when the service validates the request body then a 400 error will be returned with an appropriate message")
@@ -34,10 +39,7 @@ public class SGGCControllerSpringBootTest {
         GetCommonGamesRequest sggcRequest = new GetCommonGamesRequest();
         sggcRequest.setMultiplayerOnly(true);
         sggcRequest.setSteamIds(Collections.singleton("someSteamId"));
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_JSON);
-        HttpEntity<String> entity = new HttpEntity<>(om.writeValueAsString(sggcRequest), headers);
-        ResponseEntity<String> response = restTemplate.exchange("http://localhost:" + port + "/api/sggc/", HttpMethod.POST, entity, String.class);
+
 
         final ApiError error = new ApiError(
                 "Exception",
@@ -45,8 +47,12 @@ public class SGGCControllerSpringBootTest {
                 Collections.singleton("More than one Steam id must be included")
         );
         SGGCResponse expectedResponse = new SGGCResponse(false, error);
-        assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
-        assertEquals(om.writeValueAsString(expectedResponse), response.getBody());
+
+        mockMvc.perform(post(SGGC_CONTROLLER_URI)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(mapper.writeValueAsString(sggcRequest)))
+                .andExpect(status().isBadRequest())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.content().json(mapper.writeValueAsString(expectedResponse), false));
     }
 }
-*/
