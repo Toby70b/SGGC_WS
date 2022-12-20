451ee8d439375ab3561eea31cea4d49001479bf9
Toby <tobypl407@googlemail.com>
2022-12-20 10:13:26 +0000

    Remove pre-configuration of wiremock stubs

--- src/test/java/com/sggc/WiremockContainer.java
+++ src/test/java/com/sggc/WiremockContainer.java
@@ -1,7 +1,6 @@
 package com.sggc;
 
 import com.github.tomakehurst.wiremock.client.WireMock;
-import org.testcontainers.containers.BindMode;
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.utility.DockerImageName;
 
@@ -9,14 +8,10 @@ public class WiremockContainer extends GenericContainer<WiremockContainer>  {
 
     private static final String DEFAULT_DOCKER_IMAGE = "wiremock/wiremock:latest";
     private static final int DEFAULT_EXPOSED_PORT = 8080;
-    private static final String PRE_CONFIGURED_STUBS_HOST_PATH = "/wiremock";
-    private static final String PRE_CONFIGURED_STUBS_CONTAINER_PATH = "/home/wiremock";
 
     public WiremockContainer() {
         super(DockerImageName.parse(DEFAULT_DOCKER_IMAGE));
-        this.withExposedPorts(DEFAULT_EXPOSED_PORT)
-                .withClasspathResourceMapping(PRE_CONFIGURED_STUBS_HOST_PATH,
-                        PRE_CONFIGURED_STUBS_CONTAINER_PATH,BindMode.READ_ONLY);
+        this.withExposedPorts(DEFAULT_EXPOSED_PORT);
     }
 
     /**
