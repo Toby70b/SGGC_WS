db146c9ddef394b7f9d07da2ee9473b30549d6cb
Toby <tobypl407@googlemail.com>
2022-12-20 12:33:57 +0000

    Add bind for Mock Response Body Files to Testcontainer setup

--- src/test/java/com/sggc/WiremockContainer.java
+++ src/test/java/com/sggc/WiremockContainer.java
@@ -1,6 +1,7 @@
 package com.sggc;
 
 import com.github.tomakehurst.wiremock.client.WireMock;
+import org.testcontainers.containers.BindMode;
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.utility.DockerImageName;
 
@@ -8,10 +9,14 @@ public class WiremockContainer extends GenericContainer<WiremockContainer>  {
 
     private static final String DEFAULT_DOCKER_IMAGE = "wiremock/wiremock:latest";
     private static final int DEFAULT_EXPOSED_PORT = 8080;
+    private static final String FILES_HOST_PATH = "/wiremock/files";
+    private static final String FILES_CONTAINER_PATH = "/home/wiremock/__files";
 
     public WiremockContainer() {
         super(DockerImageName.parse(DEFAULT_DOCKER_IMAGE));
-        this.withExposedPorts(DEFAULT_EXPOSED_PORT);
+        this.withExposedPorts(DEFAULT_EXPOSED_PORT)
+                .withClasspathResourceMapping(FILES_HOST_PATH,
+                        FILES_CONTAINER_PATH,BindMode.READ_ONLY);
     }
 
     /**
