8b3cd1a64cc53e0fc0e342d68101c86338558d86
Toby <tobypl407@googlemail.com>
2022-12-24 12:16:01 +0000

    Updating unit tests and adding new Integration tests for SGGCController

--- src/test/java/com/sggc/controllers/SggcControllerIT.java
+++ src/test/java/com/sggc/controllers/SggcControllerIT.java
@@ -2,6 +2,9 @@ package com.sggc.controllers;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sggc.errors.ApiError;
+import com.sggc.exceptions.TooFewSteamIdsException;
+import com.sggc.exceptions.UserHasNoGamesException;
+import com.sggc.exceptions.VanityUrlResolutionException;
 import com.sggc.models.sggc.SggcResponse;
 import com.sggc.models.steam.request.GetCommonGamesRequest;
 import com.sggc.services.GameService;
@@ -10,17 +13,14 @@ import com.sggc.services.VanityUrlService;
 import com.sggc.validation.ValidationResult;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Set;
@@ -40,14 +40,12 @@ public class SggcControllerIT {
     private MockMvc mockMvc;
     @Autowired
     private ObjectMapper mapper;
-    @Autowired
-    private SggcController controller;
-    @MockBean
-    private GameService gameService;
     @MockBean
     private UserService userService;
     @MockBean
     private VanityUrlService vanityUrlService;
+    @MockBean
+    private GameService gameService;
 
     @Test
     @DisplayName("Given a request with less than two Steam id's when validating the request body then a 400 error will be returned with an appropriate message")
@@ -78,12 +76,12 @@ public class SggcControllerIT {
     public void givenARRequestIsReceivedWithAnInvalidVanityUrlWhenValidatingThenA400ErrorWillBeReturnedWithAnAppropriateResponseObject() throws Exception {
         GetCommonGamesRequest sggcRequest = new GetCommonGamesRequest();
         sggcRequest.setMultiplayerOnly(true);
-        String mockInvalidVanityUrl = "someVanityUrl!%$%£%$R";
+        String mockVanityUrl = "someVanityUrl!%$%£%$R";
         String mockSteamId = "someSteamId";
-        sggcRequest.setSteamIds(Set.of(mockInvalidVanityUrl,mockSteamId));
+        sggcRequest.setSteamIds(Set.of(mockVanityUrl,mockSteamId));
 
-        ValidationResult expectedValidationError = new ValidationResult(true, mockInvalidVanityUrl, VANITY_URL_NOT_ALPHANUMERIC_ERROR_MESSAGE);
-        when(vanityUrlService.validateSteamIdsAndVanityUrls(Set.of(mockInvalidVanityUrl,mockSteamId))).thenReturn(List.of(expectedValidationError));
+        ValidationResult expectedValidationError = new ValidationResult(true, mockVanityUrl, VANITY_URL_NOT_ALPHANUMERIC_ERROR_MESSAGE);
+        when(vanityUrlService.validateSteamIdsAndVanityUrls(Set.of(mockVanityUrl,mockSteamId))).thenReturn(List.of(expectedValidationError));
 
 
         final ApiError error = new ApiError(
@@ -101,8 +99,122 @@ public class SggcControllerIT {
                 .andExpect(MockMvcResultMatchers.content().json(mapper.writeValueAsString(expectedResponse), false));
     }
 
-    //check json for validation error
-    //check json for vanity url resolution error
-    //check json for user has no games error
-    //check a 500 error
+    @Test
+    @DisplayName("Given a request with a valid Vanity URL which does not have a corresponding Steam user id when resolving " +
+            "it via the Steam API then a 400 error will be return with an appropriate response object")
+    public void givenARRequestWithAVValidVanityUrlWhichDoesNotHaveACorrespondingSteamUserIdWhenResolvingThenItWillReturnA400Error() throws Exception {
+        GetCommonGamesRequest sggcRequest = new GetCommonGamesRequest();
+        sggcRequest.setMultiplayerOnly(true);
+        String mockInvalidVanityUrl = "someVanityUrl";
+        String mockSteamId = "someSteamId";
+        sggcRequest.setSteamIds(Set.of(mockInvalidVanityUrl,mockSteamId));
+
+        when(vanityUrlService.validateSteamIdsAndVanityUrls(Set.of(mockInvalidVanityUrl,mockSteamId))).thenReturn(new ArrayList<>());
+
+        when(vanityUrlService.resolveVanityUrls(Set.of(mockInvalidVanityUrl,mockSteamId)))
+                .thenThrow(new VanityUrlResolutionException(mockInvalidVanityUrl));
+
+        final ApiError error = new ApiError(
+                "VanityUrlResolutionException",
+                "Vanity Url: someVanityUrl could not be resolved to a steam id",
+                null
+        );
+        SggcResponse expectedResponse = new SggcResponse(false, error);
+
+        mockMvc.perform(post(SGGC_CONTROLLER_URI)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(mapper.writeValueAsString(sggcRequest)))
+                .andExpect(status().isNotFound())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.content().json(mapper.writeValueAsString(expectedResponse), false));
+    }
+
+    @Test
+    @DisplayName("Given a request with a valid Steam user id, when a user is found to own no games return a 404 error with an appropriate response object")
+    public void givenARequestWithAValidSteamUserIdWhenAUserIdFoundToOwnNoGamesReturnA404ErrorWithAnAppropriateResponseObject() throws Exception {
+        GetCommonGamesRequest sggcRequest = new GetCommonGamesRequest();
+        sggcRequest.setMultiplayerOnly(true);
+        String mockSteamId1 = "someSteamId1";
+        String mockSteamId2 = "someSteamId2";
+        sggcRequest.setSteamIds(Set.of(mockSteamId1,mockSteamId2));
+
+        when(vanityUrlService.validateSteamIdsAndVanityUrls(Set.of(mockSteamId1,mockSteamId2))).thenReturn(new ArrayList<>());
+
+        when(vanityUrlService.resolveVanityUrls(Set.of(mockSteamId1,mockSteamId2))).thenReturn(Set.of(mockSteamId1,mockSteamId2));
+
+        when(userService.getIdsOfGamesOwnedByAllUsers(Set.of(mockSteamId1,mockSteamId2)))
+                .thenThrow(new UserHasNoGamesException(mockSteamId1));
+
+        final ApiError error = new ApiError(
+                "UserHasNoGamesException",
+                "User with Id: someSteamId1 has no games associated with their account, or doesn't exist.",
+                null
+        );
+        SggcResponse expectedResponse = new SggcResponse(false, error);
+
+        mockMvc.perform(post(SGGC_CONTROLLER_URI)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(mapper.writeValueAsString(sggcRequest)))
+                .andExpect(status().isNotFound())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.content().json(mapper.writeValueAsString(expectedResponse), false));
+    }
+
+    @Test
+    @DisplayName("Given a request when after Vanity URL resolution there are fewer than two Steam ids to find common games for" +
+            " then a 400 error will be return with an appropriate response object")
+    public void givenARequestWhenAfterResolutionThereAreFewerThanTwoSteamIdsToFindCommonGamesForThenReturnA400ErrorWithAnAppropriateResponseObject() throws Exception {
+        GetCommonGamesRequest sggcRequest = new GetCommonGamesRequest();
+        sggcRequest.setMultiplayerOnly(true);
+        String mockSteamId1 = "someSteamId1";
+        String mockSteamId2 = "someSteamId2";
+        sggcRequest.setSteamIds(Set.of(mockSteamId1,mockSteamId2));
+
+        when(vanityUrlService.validateSteamIdsAndVanityUrls(Set.of(mockSteamId1,mockSteamId2))).thenReturn(new ArrayList<>());
+
+        when(vanityUrlService.resolveVanityUrls(Set.of(mockSteamId1,mockSteamId2))).thenReturn(Set.of(mockSteamId1,mockSteamId2));
+
+        when(userService.getIdsOfGamesOwnedByAllUsers(Set.of(mockSteamId1,mockSteamId2)))
+                .thenThrow(new TooFewSteamIdsException());
+
+        final ApiError error = new ApiError(
+                "TooFewSteamIdsException",
+                "A minimum of two users must be provided in order to compare game libraries. If a mix of " +
+                        "Vanity URL and Steam Id have been provided, please confirm that they dont relate to the same user.",
+                null
+        );
+        SggcResponse expectedResponse = new SggcResponse(false, error);
+
+        mockMvc.perform(post(SGGC_CONTROLLER_URI)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(mapper.writeValueAsString(sggcRequest)))
+                .andExpect(status().isBadRequest())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.content().json(mapper.writeValueAsString(expectedResponse), false));
+    }
+
+    @Test
+    @DisplayName("Given a request when an uncaught exception is thrown then a 500 error will be return with an appropriate response object")
+    public void givenARequestWhenAnUncaughtExceptionIsThrownThenA500ErrorWillBeReturnedWithAnAppropriateResponseObject() throws Exception {
+        GetCommonGamesRequest sggcRequest = new GetCommonGamesRequest();
+        sggcRequest.setMultiplayerOnly(true);
+        String mockSteamId1 = "someSteamId1";
+        String mockSteamId2 = "someSteamId2";
+        sggcRequest.setSteamIds(Set.of(mockSteamId1,mockSteamId2));
+
+        when(vanityUrlService.validateSteamIdsAndVanityUrls(Set.of(mockSteamId1,mockSteamId2))).thenThrow(new RuntimeException());
+
+        final ApiError error = new ApiError(
+                "Exception",
+                "Internal server error."
+        );
+        SggcResponse expectedResponse = new SggcResponse(false, error);
+
+        mockMvc.perform(post(SGGC_CONTROLLER_URI)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(mapper.writeValueAsString(sggcRequest)))
+                .andExpect(status().isInternalServerError())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.content().json(mapper.writeValueAsString(expectedResponse), false));
+    }
 }
--- src/test/java/com/sggc/controllers/SggcControllerTest.java
+++ src/test/java/com/sggc/controllers/SggcControllerTest.java
@@ -18,6 +18,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -41,7 +42,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @ExtendWith(MockitoExtension.class)
 class SggcControllerTest {
 
-
     @Mock
     private GameService gameService;
 
@@ -60,7 +60,9 @@ class SggcControllerTest {
         when(userService.getIdsOfGamesOwnedByAllUsers(any())).thenReturn(new HashSet<>());
         Game exampleGame = new Game();
         when(gameService.findGamesById(any(), anyBoolean())).thenReturn(Set.of(exampleGame));
+
         ResponseEntity<SggcResponse> response = sggcController.getGamesAllUsersOwn(new GetCommonGamesRequest());
+
         assertEquals(HttpStatus.OK, response.getStatusCode());
         assertNotNull(response.getBody());
         assertNotNull(response.getBody().getBody());
@@ -68,146 +70,94 @@ class SggcControllerTest {
         assertTrue(response.getBody().isSuccess());
     }
 
-    @Nested
-    @DisplayName("Controller Advice Tests")
-    class ControllerAdviceTests {
 
-        private MockMvc mockMvc;
+    @Test
+    @DisplayName("If one of the users specified in the request has no games it will return a 404 error with an appropriate message")
+    void IfOneOfTheUsersSpecifiedInTheRequestHasNoGamesItWillReturnA404ErrorWithAnAppropriateMessage() throws Exception {
+        ObjectMapper objectMapper = new ObjectMapper();
 
-        @BeforeEach
-        void setUp() {
-            this.mockMvc = MockMvcBuilders.standaloneSetup(new SggcController(gameService, userService, vanityUrlService))
-                    .setControllerAdvice(SggcRestExceptionHandler.class)
-                    .build();
-        }
+        when(userService.getIdsOfGamesOwnedByAllUsers(Set.of("76561198045206222", "76561198045206223")))
+                .thenThrow(new UserHasNoGamesException("76561198045206222"));
 
-        @Test
-        @DisplayName("If one of the users specified in the request has no games it will return a 404 error with an appropriate message")
-        void IfOneOfTheUsersSpecifiedInTheRequestHasNoGamesItWillReturnA404ErrorWithAnAppropriateMessage() throws Exception {
-            ObjectMapper objectMapper = new ObjectMapper();
+        when(vanityUrlService.resolveVanityUrls(Set.of("76561198045206222", "76561198045206223")))
+                .thenReturn(Set.of("76561198045206222", "76561198045206223"));
 
-            when(userService.getIdsOfGamesOwnedByAllUsers(Set.of("76561198045206222", "76561198045206223")))
-                    .thenThrow(new UserHasNoGamesException("76561198045206222"));
+        GetCommonGamesRequest request = new GetCommonGamesRequest();
+        request.setSteamIds(Set.of("76561198045206222", "76561198045206223"));
+        request.setMultiplayerOnly(true);
 
-            when(vanityUrlService.resolveVanityUrls(Set.of("76561198045206222", "76561198045206223")))
-                    .thenReturn(Set.of("76561198045206222", "76561198045206223"));
+        ResponseEntity<SggcResponse> response = sggcController.getGamesAllUsersOwn(request);
 
+        SggcResponse expectedResponse = new SggcResponse(false, new ApiError("UserHasNoGamesException",
+                "User with Id: 76561198045206222 has no games associated with their account, or doesn't exist.", null));
 
-            GetCommonGamesRequest request = new GetCommonGamesRequest();
-            request.setSteamIds(Set.of("76561198045206222", "76561198045206223"));
-            request.setMultiplayerOnly(true);
-
-            MvcResult result = this.mockMvc
-                    .perform(MockMvcRequestBuilders.post("/api/sggc/")
-                            .contentType(MediaType.APPLICATION_JSON)
-                            .content(objectMapper.writeValueAsString(request)))
-                    .andExpect(status().isNotFound())
-                    .andExpect(MockMvcResultMatchers.status().isNotFound())
-                    .andReturn();
-
-            SggcResponse expectedResponse = new SggcResponse(false, new ApiError("UserHasNoGamesException",
-                    "User with Id: 76561198045206222 has no games associated with their account, or doesn't exist.", null));
-            //Convert the expected response to JSON and then convert it back to an object to match actual response
-            String expectedResponseJson = objectMapper.writeValueAsString(expectedResponse);
-            SggcResponse expectedResponseObj = objectMapper.readValue(expectedResponseJson, SggcResponse.class);
-
-            SggcResponse actualResponse = objectMapper.readValue(result.getResponse().getContentAsString(), SggcResponse.class);
-            assertEquals(expectedResponseObj, actualResponse);
-        }
-
-        @Test
-        @DisplayName("If one of the Steam vanity URLs specified in the request is invalid it will return a 400 error with an appropriate message")
-        void IfOneOfTheSteamIdsSpecifiedInTheRequestIsInvalidItWillReturnA400ErrorWithAnAppropriateMessage() throws Exception {
-            ObjectMapper objectMapper = new ObjectMapper();
-
-            List<ValidationResult> validationErrors = List.of(new ValidationResult(true,
-                    "765611980452$062222321321", VANITY_URL_NOT_ALPHANUMERIC_ERROR_MESSAGE));
-
-            when(vanityUrlService.validateSteamIdsAndVanityUrls(Set.of("765611980452062222321321", "76561198045206223")))
-                    .thenReturn(validationErrors);
-
-            GetCommonGamesRequest request = new GetCommonGamesRequest();
-            request.setSteamIds(Set.of("765611980452062222321321", "76561198045206223"));
-            request.setMultiplayerOnly(true);
-
-            MvcResult result = this.mockMvc
-                    .perform(MockMvcRequestBuilders.post("/api/sggc/")
-                            .contentType(MediaType.APPLICATION_JSON)
-                            .content(objectMapper.writeValueAsString(request)))
-                    .andExpect(status().isBadRequest())
-                    .andExpect(MockMvcResultMatchers.status().isBadRequest())
-                    .andReturn();
-
-            SggcResponse expectedResponse = new SggcResponse(false, new ApiError("ValidationException",
-                    "Request body violates validation rules, check error details for more information.", validationErrors));
-            //Convert the expected response to JSON and then convert it back to an object to match actual response
-            String expectedResponseJson = objectMapper.writeValueAsString(expectedResponse);
-            SggcResponse expectedResponseObj = objectMapper.readValue(expectedResponseJson, SggcResponse.class);
-
-            SggcResponse actualResponse = objectMapper.readValue(result.getResponse().getContentAsString(), SggcResponse.class);
-            assertEquals(expectedResponseObj, actualResponse);
-        }
-
-        @Test
-        @DisplayName("If an error occurs while trying to retrieve the steam key secret it will return a 500 error with an appropriate message")
-        void IfAnErrorOccursWhileTryingToRetrieveTheSteamKeySecretItWillReturnA500ErrorWithAnAppropriateMessage() throws Exception {
-            ObjectMapper objectMapper = new ObjectMapper();
-
-            when(vanityUrlService.resolveVanityUrls(Set.of("765611980452062222321321", "76561198045206223")))
-                    .thenThrow(new SecretRetrievalException(new Exception()));
-
-            GetCommonGamesRequest request = new GetCommonGamesRequest();
-            request.setSteamIds(Set.of("765611980452062222321321", "76561198045206223"));
-            request.setMultiplayerOnly(true);
-
-            MvcResult result = this.mockMvc
-                    .perform(MockMvcRequestBuilders.post("/api/sggc/")
-                            .contentType(MediaType.APPLICATION_JSON)
-                            .content(objectMapper.writeValueAsString(request)))
-                    .andExpect(status().isInternalServerError())
-                    .andExpect(MockMvcResultMatchers.status().isInternalServerError())
-                    .andReturn();
-
-            SggcResponse expectedResponse = new SggcResponse(false, new ApiError("Exception",
-                    "Internal server error."));
-            //Convert the expected response to JSON and then convert it back to an object to match actual response
-            String expectedResponseJson = objectMapper.writeValueAsString(expectedResponse);
-            SggcResponse expectedResponseObj = objectMapper.readValue(expectedResponseJson, SggcResponse.class);
-
-            SggcResponse actualResponse = objectMapper.readValue(result.getResponse().getContentAsString(), SggcResponse.class);
-            assertEquals(expectedResponseObj, actualResponse);
-        }
-
-        @Test
-        @DisplayName("If an error occurs while trying to resolve a vanity URL into a steam user id then the controller will return a 404 error with an appropriate message")
-        void IfAnErrorOccursWhileTryingToResolveAVanityUrlIntoASteamUserIdThenTheControllerWillReturnA404ErrorWithAnAppropriateMessage() throws Exception {
-            ObjectMapper objectMapper = new ObjectMapper();
-
-            when(vanityUrlService.resolveVanityUrls(Set.of("SomeVanityUrl", "76561198045206223")))
-                    .thenThrow(new VanityUrlResolutionException("SomeVanityUrl"));
-
-            GetCommonGamesRequest request = new GetCommonGamesRequest();
-            request.setSteamIds(Set.of("SomeVanityUrl", "76561198045206223"));
-            request.setMultiplayerOnly(true);
-
-            MvcResult result = this.mockMvc
-                    .perform(MockMvcRequestBuilders.post("/api/sggc/")
-                            .contentType(MediaType.APPLICATION_JSON)
-                            .content(objectMapper.writeValueAsString(request)))
-                    .andExpect(status().isNotFound())
-                    .andExpect(MockMvcResultMatchers.status().isNotFound())
-                    .andReturn();
-
-            SggcResponse expectedResponse = new SggcResponse(false, new ApiError("VanityUrlResolutionException",
-                    "Vanity Url: SomeVanityUrl could not be resolved to a steam id"));
-            //Convert the expected response to JSON and then convert it back to an object to match actual response
-            String expectedResponseJson = objectMapper.writeValueAsString(expectedResponse);
-            SggcResponse expectedResponseObj = objectMapper.readValue(expectedResponseJson, SggcResponse.class);
-
-            SggcResponse actualResponse = objectMapper.readValue(result.getResponse().getContentAsString(), SggcResponse.class);
-            assertEquals(expectedResponseObj, actualResponse);
-        }
-
+        assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
+        assertNotNull(response.getBody());
+        assertEquals(expectedResponse, response.getBody());
     }
 
+    @Test
+    @DisplayName("If one of the Steam vanity URLs specified in the request is invalid it will return a 400 error with an appropriate message")
+    void IfOneOfTheSteamIdsSpecifiedInTheRequestIsInvalidItWillReturnA400ErrorWithAnAppropriateMessage() throws Exception {
+        List<ValidationResult> validationErrors = List.of(new ValidationResult(true,
+                "765611980452$062222321321", VANITY_URL_NOT_ALPHANUMERIC_ERROR_MESSAGE));
+
+        when(vanityUrlService.validateSteamIdsAndVanityUrls(Set.of("765611980452062222321321", "76561198045206223")))
+                .thenReturn(validationErrors);
+
+        GetCommonGamesRequest request = new GetCommonGamesRequest();
+        request.setSteamIds(Set.of("765611980452062222321321", "76561198045206223"));
+        request.setMultiplayerOnly(true);
+
+        ResponseEntity<SggcResponse> response = sggcController.getGamesAllUsersOwn(request);
+
+        SggcResponse expectedResponse = new SggcResponse(false, new ApiError("ValidationException",
+                "Request body violates validation rules, check error details for more information.", validationErrors));
+
+        assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+        assertNotNull(response.getBody());
+        assertEquals(expectedResponse, response.getBody());
+    }
+
+    @Test
+    @DisplayName("If an error occurs while trying to retrieve the steam key secret it throw an appropriate exception")
+    void IfAnErrorOccursWhileTryingToRetrieveTheSteamKeySecretItWillReturnA500ErrorWithAnAppropriateMessage() throws Exception {
+        when(vanityUrlService.resolveVanityUrls(Set.of("765611980452062222321321", "76561198045206223")))
+                .thenThrow(new SecretRetrievalException(new Exception()));
+
+        GetCommonGamesRequest request = new GetCommonGamesRequest();
+        request.setSteamIds(Set.of("765611980452062222321321", "76561198045206223"));
+        request.setMultiplayerOnly(true);
+
+        SecretRetrievalException expectedException =
+                assertThrows(SecretRetrievalException.class, () ->
+                        sggcController.getGamesAllUsersOwn(request));
+
+        SggcResponse expectedResponse = new SggcResponse(false, new ApiError("Exception",
+                "Internal server error."));
+
+        assertEquals("Exception occurred when attempting to retrieve a secret from AWS secrets manager", expectedException.getMessage());
+    }
+
+    @Test
+    @DisplayName("If an error occurs while trying to resolve a vanity URL into a steam user id then the controller will return a 404 error with an appropriate message")
+    void IfAnErrorOccursWhileTryingToResolveAVanityUrlIntoASteamUserIdThenTheControllerWillReturnA404ErrorWithAnAppropriateMessage() throws Exception {
+        when(vanityUrlService.resolveVanityUrls(Set.of("SomeVanityUrl", "76561198045206223")))
+                .thenThrow(new VanityUrlResolutionException("SomeVanityUrl"));
+
+        GetCommonGamesRequest request = new GetCommonGamesRequest();
+        request.setSteamIds(Set.of("SomeVanityUrl", "76561198045206223"));
+        request.setMultiplayerOnly(true);
+
+        ResponseEntity<SggcResponse> response = sggcController.getGamesAllUsersOwn(request);
+
+        SggcResponse expectedResponse = new SggcResponse(false, new ApiError("VanityUrlResolutionException",
+                "Vanity Url: SomeVanityUrl could not be resolved to a steam id"));
+
+        assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
+        assertNotNull(response.getBody());
+        assertEquals(expectedResponse, response.getBody());
+    }
 }
+
+
