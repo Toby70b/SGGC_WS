c5eb81a4004c3320bc459531148a73e3803376da
Toby <tobypl407@googlemail.com>
2022-12-30 11:18:15 +0000

    Fix javadoc

--- src/main/java/com/sggc/config/awssecretmanager/SecretManagerConfig.java
+++ src/main/java/com/sggc/config/awssecretmanager/SecretManagerConfig.java
@@ -3,16 +3,15 @@ package com.sggc.config.awssecretmanager;
 import com.amazonaws.auth.AWSCredentialsProvider;
 import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
 import com.amazonaws.client.builder.AwsClientBuilder;
-import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
-import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
 import com.amazonaws.services.secretsmanager.AWSSecretsManager;
 import com.amazonaws.services.secretsmanager.AWSSecretsManagerClientBuilder;
-import com.sggc.config.dynamodb.DynamoDbProperties;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+/*
+ * Represents the configuration for AWS Secrets Manager
+ */
 @Configuration
 @RequiredArgsConstructor
 public class SecretManagerConfig {
@@ -26,7 +25,7 @@ public class SecretManagerConfig {
     /**
      * Creates a new instance of the AWS Secrets Manager client to perform actions on AWS secrets.
      * <p/>
-     * If the 'ENVIRONMENT' environment variable is equal to 'local' then the instance will be configured to connect to
+     * If the 'SECRETS_MANAGER_ADDRESS' environment variable is set then the instance will be configured to connect to
      * a local AWS Secret Manager instance running on the host machine.
      *
      * @return a new instance of the AWS Secrets Manager client
